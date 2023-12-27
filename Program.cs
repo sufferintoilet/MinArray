772a3c0fb6cf421ac48f60c4bf6a7bf9ea887dc9
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2023-12-27 23:28:11 +0900

    MinArray

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array= {1,2,3,4,5,6,7,8,9,20,21};
+        Console.WriteLine("Gia tri nho nhat la");
+        Console.WriteLine(MinFind(array));
+    }
+    static int MinFind(int[] array)
+    {
+        int min = array[0];
+        for (int i = 0; i < array.Length-1; i++)
+        {
+            if(array[1]<min)
+            {
+                min=array[i];
+            }
+        }
+        return min;
+    }
+}
